a076d5b8735a060cb79418573aefca4801299221
Brandon EtteR <brandonetter@gmail.com>
2023-06-13 22:46:15 -0400

    ready

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 
-const testAddon = require("./build/Release/testaddon.node")
-console.log("addon", testAddon)
-console.log("addon", testAddon.hello("a"))
-console.log("addon", testAddon.sort(["d", "b", "c"]))
-console.log("addon", testAddon.fuzzySearch("hello", ["halo", "bello", "helli", "halp"], 0.7))
-module.exports = testAddon
+const fuzzySearch = require("./build/Release/testaddon.node")
+module.exports = fuzzySearch.fuzzySearch;
